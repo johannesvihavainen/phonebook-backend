a96c8a30e80477e105cc31b7471879818a1b2515
Johannes Vihavainen <johannesvihavainen@icloud.com>
2025-03-05 15:46:05 +0200

    add phonebook database step 8 - 3.20*

--- models/person.js
+++ models/person.js
@@ -6,6 +6,16 @@ const personSchema = new mongoose.Schema({
         minLength: 3,
         required: true
     },
+    number: {
+        type: String,
+        required: true,
+        validate: {
+          validator: function (v) {
+            return new RegExp('^\\d{2,3}-\\d+$').test(v)
+          },
+          message: props => `${props.value} is not a valid phone number! Format must be XX-XXXXXXX or XXX-XXXXXXXX.`
+        },
+      },
 })
 
 const Person = mongoose.model('Person', personSchema)
